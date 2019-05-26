466f655cc064471d2e09e26622417b038838d19c
Lan Nguyen <nguyenlan13@gmail.com>
2019-05-26 00:39:20 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,8 +5,8 @@ class Backer
     @backed_projects = []
   end
   
-  def back_project(title)
+  def back_project(Project)
 
-    @backed_projects << Project("title")
+    @backed_projects << Project.new
   end
 end
